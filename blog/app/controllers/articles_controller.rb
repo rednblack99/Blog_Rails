834c9251a20fabef42c608de40f3515e6207320e
Adam Lodowski <adamlodowski@gmail.com>
2019-03-28 17:06:03 +0000

    Added functionality for editing articles

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])    
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -21,6 +25,16 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end  
 
   private
   def article_params
